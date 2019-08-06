494f6d53dbf258e4def6a27200da06799a26d0d7
Matthew Phillips <phillipsm2@gmail.com>
2019-08-06 07:37:55 -0400

    Fill out to get this working more

--- test/lit/app.js
+++ test/lit/app.js
@@ -1,12 +1,12 @@
 import { LitElement, html } from 'https://unpkg.com/lit-element@2.2.0/lit-element.js?module';
+import Base from './make-base.js';
 
-class MyElement extends LitElement {
-
-  render() {
-    return html`
-      <p>A paragraph</p>
-    `;
-  }
+function App() {
+  return html`
+    <p>A paragraph</p>
+  `;
 }
+
+const MyElement = Base(App);
 
 customElements.define('my-element', MyElement);
